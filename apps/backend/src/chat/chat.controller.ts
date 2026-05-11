0c85ca62aaee25aa9ad14702e256eaed4158f97b
chitrank2050 <chitrank2050@gmail.com>
2026-05-11 10:41:39 +0530

    refactor: remove unused Request import from chat controller

--- apps/backend/src/chat/chat.controller.ts
+++ apps/backend/src/chat/chat.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Get, Body, Param, Request } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ApiResponse } from '../common/responses/api-response';
 
